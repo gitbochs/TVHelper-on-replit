ac40a98fd232d8ff781f404a85f71fd0c5f008cf
gitbochs <gitbochs@users.noreply.github.com>
2023-10-24 20:19:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 echo "TVHelper一键搭建脚本"
 echo "脚本作者：舒夏"
-echo "GitHub开源地址：https://github.com/sxbai/TVHelper-on-replit"
+echo "GitHub开源地址：https://github.com/gitbochs/TVHelper-on-replit"
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget -O .replit https://github.com/sxbai/TVHelper-on-replit/raw/master/.replit
-wget -O replit.nix https://github.com/sxbai/TVHelper-on-replit/raw/master/replit.nix
+wget -O .replit https://github.com/gitbochs/TVHelper-on-replit/raw/master/.replit
+wget -O replit.nix https://github.com/gitbochs/TVHelper-on-replit/raw/master/replit.nix
 cd ..
-wget -O main.sh https://github.com/sxbai/TVHelper-on-replit/raw/master/main.sh
+wget -O main.sh https://github.com/gitbochs/TVHelper-on-replit/raw/master/main.sh
 cp -r build/.replit . && cp -r build/replit.nix .
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
