1a6d5f6f91a39307fa5d004eb54c90c2442e1583
gitbochs <gitbochs@users.noreply.github.com>
2023-10-25 13:21:03 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-tag1=$(wget -qO- -t1 -T2 "https://api.github.com/repos/sec-an/TVHelper/releases/latest" | jq -r '.tag_name')
-wget -O TVHelper_Linux_x86_64.tar.gz https://github.com/gitbochs/TVHelper/releases/download/${tag1}/TVHelper_Linux_x86_64.tar.gz \
+tag1=$(wget -qO- -t1 -T2 "https://api.github.com/repos/gitboch/TVHelper/releases/latest" | jq -r '.tag_name')
+wget -O TVHelper_Linux_x86_64.tar.gz https://github.com/gitbochs/TVHelper/releases/download/${tag1}/TVHelper_Linux_x86_64.tar.xz \
 && tar -zxvf TVHelper_Linux_x86_64.tar.gz
 chmod +x TVHelper && rm -rf TVHelper_Linux_x86_64.tar.gz
 ./TVHelper
